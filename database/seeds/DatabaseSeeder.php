4b00ccd6e2b2526a9bc2c0fffdeda513e9680f7b
高旭辉 <519996283@qq.com>
2018-05-07 16:28:42 +0800

    M

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         Model::unguard();
 
-        $this->call(UserTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
 
         Model::reguard();
 
